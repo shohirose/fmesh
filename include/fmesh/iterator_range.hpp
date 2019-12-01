47f3e292aee989c1f439f3ccd09eb578e8068cfd
Sho Hirose <sho.hirose@gmail.com>
2019-12-01 12:22:11 -0600

    fix: use snake_case instead of CamelCase in iterator_range.hpp.

--- include/fmesh/iterator_range.hpp
+++ include/fmesh/iterator_range.hpp
@@ -1,33 +1,55 @@
-#ifndef ITERATOR_RANGE_HPP
-#define ITERATOR_RANGE_HPP
+// MIT License
+//
+// Copyright (c) 2019 Sho Hirose (sho.hirose@gmail.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef FMESH_ITERATOR_RANGE_HPP
+#define FMESH_ITERATOR_RANGE_HPP
 
 #include <cassert>
 
-#include "IndexIterator.hpp"
+#include "fmesh/index_iterator.hpp"
 
-namespace shirose {
+namespace fmesh {
 
 template <typename Iterator>
-class IteratorRange {
+class iterator_range {
  public:
-  IteratorRange() = default;
-  IteratorRange(const Iterator& begin, const Iterator& end)
+  iterator_range() = default;
+  iterator_range(const Iterator& begin, const Iterator& end)
       : begin_{begin}, end_{end} {}
 
-  auto Begin() const noexcept { return begin_; }
-  auto End() const noexcept { return end_; }
-  auto Size() const noexcept {
+  auto begin() const noexcept { return begin_; }
+  auto end() const noexcept { return end_; }
+  auto size() const noexcept {
     assert(begin_ <= end_);
     return static_cast<std::size_t>(std::distance(begin_, end_));
   }
-  bool Empty() const noexcept { return begin_ == end_; }
+  bool empty() const noexcept { return begin_ == end_; }
 
-  friend bool operator==(const IteratorRange& left,
-                         const IteratorRange& right) noexcept {
+  friend bool operator==(const iterator_range& left,
+                         const iterator_range& right) noexcept {
     return left.begin_ == right.begin_ && left.end_ == right.end_;
   }
-  friend bool operator!=(const IteratorRange& left,
-                         const IteratorRange& right) noexcept {
+  friend bool operator!=(const iterator_range& left,
+                         const iterator_range& right) noexcept {
     return !(left == right);
   }
 
@@ -37,11 +59,11 @@ class IteratorRange {
 };
 
 template <typename Iterator>
-IteratorRange<Iterator> MakeIteratorRange(const Iterator& begin,
-                                          const Iterator& end) {
+iterator_range<Iterator> make_iterator_range(const Iterator& begin,
+                                             const Iterator& end) {
   return {begin, end};
 }
 
-}  // namespace shirose
+}  // namespace fmesh
 
-#endif  // ITERATOR_RANGE_HPP
+#endif  // FMESH_ITERATOR_RANGE_HPP
