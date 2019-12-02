4df74971d43a1611fffbc20ddfb8d392c987fa99
Sho Hirose <sho.hirose@gmail.com>
2019-12-02 08:29:20 -0600

    feat: add invalidate functions to fracture_mesh.

--- include/fmesh/fracture_mesh.hpp
+++ include/fmesh/fracture_mesh.hpp
@@ -20,8 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include <vector>
 #include <algorithm>
+#include <vector>
 #include "fmesh/edge.hpp"
 #include "fmesh/index.hpp"
 #include "fmesh/iterator_range.hpp"
@@ -32,10 +32,25 @@ namespace fmesh {
 template <typename Point, typename Face>
 class fracture_mesh {
  public:
+  /// @brief Returns the number of vertices
   auto num_vertices() const noexcept { return vertices_.size(); }
+
+  /// @brief Returns the number of edges
   auto num_edges() const noexcept { return edges_.size(); }
+
+  /// @brief Returns the number of faces
   auto num_faces() const noexcept { return faces_.size(); }
 
+  /// @brief Checks if a vertex is valid
+  bool is_valid(vertex_index i) const noexcept { return is_valid_vertex_[i]; }
+
+  /// @brief Checks if an edge is valid
+  bool is_valid(edge_index i) const noexcept { return is_valid_edge_[i]; }
+
+  /// @brief Checks if a face is valid
+  bool is_valid(face_index i) const noexcept { return is_valid_face_[i]; }
+
+  /// @brief Find the edge index of a given edge
   edge_index find(const fmesh::edge& e) const noexcept {
     const auto& es = vertex_edges_[e.first];
     const auto it = std::find_if(es.begin(), es.end(), [e, this](auto ei) {
@@ -44,6 +59,7 @@ class fracture_mesh {
     return it == es.end() ? edge_index{} : *it;
   }
 
+  /// @brief Find the face index of a given face
   face_index find(const Face& f) const noexcept {
     const auto& fs = vertex_faces_[f[0]];
     const auto it = std::find_if(fs.begin(), fs.end(), [&f, this](auto fi) {
@@ -52,6 +68,11 @@ class fracture_mesh {
     return it == fs.end() ? face_index{} : *it;
   }
 
+  /// @brief Add a new vertex to mesh
+  /// @param[in] p A new vertex as a vector
+  /// @return The index of a new vertex
+  ///
+  /// This function updates vertex connectivity info.
   vertex_index add(const Point& p) {
     const vertex_index vi{vertices_.size()};
     vertices_.push_back(p);
@@ -59,9 +80,17 @@ class fracture_mesh {
     vertex_vertices_.resize(n);
     vertex_edges_.resize(n);
     vertex_faces_.resize(n);
+    is_valid_vertex_.push_back(true);
     return vi;
   }
 
+  /// @brief Add a new edge to mesh
+  /// @param[in] e A new edge
+  /// @return The index of a new edge
+  ///
+  /// This function updates edge connectivity info.
+  /// If the given edge is already registered, an invalid edge index will be
+  /// returned.
   edge_index add(const fmesh::edge& e) {
     if (this->find(e).is_valid()) {
       std::cerr << "Warning: edge [" << e << "] is already registered.\n";
@@ -71,10 +100,18 @@ class fracture_mesh {
     const edge_index ei{edges_.size()};
     edges_.push_back(e);
     edge_faces_.resize(edges_.size());
+    is_valid_edge_.push_back(true);
     this->update_edge_connectivity(e, ei);
     return ei;
   }
 
+  /// @brief Add a new face to mesh
+  /// @param[in] p A new face
+  /// @return The index of a new face
+  ///
+  /// This function updates vertex, edge, and face connectivity info.
+  /// If the given face is already registered, an invalid face index will be
+  /// returned.
   face_index add(const Face& f) {
     if (this->find(f).is_valid()) {
       std::cerr << "Warning: face [" << f << "] is already registered.\n";
@@ -84,8 +121,48 @@ class fracture_mesh {
     const face_index fi{faces_.size()};
     faces_.push_back(f);
     face_edges_.resize(faces_.size());
+    is_valid_face_.push_back(true);
     this->update_face_connectivity(f, fi);
     return fi;
+  }
+
+  /// @brief Invalidate a given vertex
+  /// @param[in] vi Vertex index to be invalidated
+  ///
+  /// This function invalidates a given vertex. In addition, it also invalidates
+  /// edges and faces connected to the vertex.
+  void invalidate(vertex_index vi) {
+    has_invalid_vertices_ = true;
+    is_valid_vertex_[vi] = false;
+
+    // Invalidate all edges connected to the vertex
+    const auto& es = vertex_edges_[vi];
+    for (auto&& ei : es) is_valid_edge_[ei] = false;
+
+    // Invalidate all faces connected to the vertex
+    const auto& fs = vertex_faces_[vi];
+    for (auto&& fi : fs) is_valid_face_[fi] = false;
+  }
+
+  /// @brief Invalidate a given edge
+  /// @param[in] vi Edge index to be invalidated
+  ///
+  /// This function invalidates a given edge. In addition, it also invalidates
+  /// faces which the edge consists of.
+  void invalidate(edge_index ei) {
+    has_invalid_edges_ = true;
+    is_valid_edge_[ei] = false;
+
+    // Invalidate all faces which the edge consists of
+    const auto& fs = edge_faces_[ei];
+    for (auto&& fi : fs) is_valid_face_[fi] = false;
+  }
+
+  /// @brief Invalidate a given face
+  /// @param[in] vi Face index to be invalidated
+  void invalidate(face_index fi) {
+    has_invalid_faces_ = true;
+    is_valid_face_[fi] = false;
   }
 
   auto vertex_begin() noexcept { return vertices_.begin(); }
@@ -129,64 +206,110 @@ class fracture_mesh {
   const auto& edge(edge_index i) const noexcept { return edges_[i]; }
   const auto& face(face_index i) const noexcept { return faces_[i]; }
 
+  bool has_invalid_entities() const noexcept {
+    return has_invalid_vertices_ || has_invalid_edges_ || has_invalid_faces_;
+  }
+
+  /// @brief Removes invalid mesh entities from arrays
+  void remove_invalid_entities();
+
  private:
-  void update_face_connectivity(const Face& f, const face_index fi) noexcept {
-    for (auto&& vi : f) vertex_faces_[vi].push_back(fi);
-    const auto fedges = f.to_edges();
-
-    for (auto&& e : fedges) {
-      const auto ei = this->find(e);
-
-      if (ei.is_valid()) {
-        // Edge already resitered
-        // So only edge-face connectivity data are updated
-        face_edges_[fi].push_back(ei);
-        edge_faces_[ei].push_back(fi);
-      } else {
-        // Edge is new
-        // So first resiter the new edge, and then update all connectivity data.
-        const edge_index ej{edges_.size()};
-        edges_.push_back(e);
-        edge_faces_.resize(edges_.size());
-
-        edge_faces_[ej].push_back(fi);
-        face_edges_[fi].push_back(ej);
-        vertex_edges_[e.first].push_back(ej);
-        vertex_edges_[e.second].push_back(ej);
-        vertex_vertices_[e.first].push_back(e.second);
-        vertex_vertices_[e.second].push_back(e.first);
-      }
-    }
-  }
-
+  void update_face_connectivity(const Face& f, const face_index fi) noexcept;
   void update_edge_connectivity(const fmesh::edge& e,
-                                const edge_index ei) noexcept {
-    // Update vertex-edges and vertex-vertices
-    vertex_edges_[e.first].push_back(ei);
-    vertex_edges_[e.second].push_back(ei);
-    vertex_vertices_[e.first].push_back(e.second);
-    vertex_vertices_[e.second].push_back(e.first);
+                                const edge_index ei) noexcept;
 
-    // Find faces connected to the edge
-    // Faces connected to both ends of the edge are the ones
-    const auto& fs1 = vertex_faces_[e.first];
-    const auto& fs2 = vertex_faces_[e.second];
-    for (auto&& fi : fs1) {
-      if (std::find(fs2.begin(), fs2.end(), fi) != fs2.end()) {
-        edge_faces_[ei].push_back(fi);
-        face_edges_[fi].push_back(ei);
-      }
-    }
-  }
-
+  // Mesh entities
   vertex_property<Point> vertices_;
   edge_property<fmesh::edge> edges_;
   face_property<Face> faces_;
+
+  // Mesh connectivity
   vertex_property<std::vector<vertex_index>> vertex_vertices_;
   vertex_property<std::vector<edge_index>> vertex_edges_;
   vertex_property<std::vector<face_index>> vertex_faces_;
   edge_property<std::vector<face_index>> edge_faces_;
   face_property<std::vector<edge_index>> face_edges_;
+
+  // Is a mesh entity valid?
+  // Mesh entities can be invalidated.
+  // Call remove_invalid_entities() if you really need to remove them.
+  vertex_property<bool> is_valid_vertex_;
+  edge_property<bool> is_valid_edge_;
+  face_property<bool> is_valid_face_;
+
+  // Flags to check if there are any invalid mesh entities.
+  bool has_invalid_vertices_;
+  bool has_invalid_edges_;
+  bool has_invalid_faces_;
 };
+
+template <typename Point, typename Face>
+void fracture_mesh<Point, Face>::update_face_connectivity(
+    const Face& f, const face_index fi) noexcept {
+  for (auto&& vi : f) vertex_faces_[vi].push_back(fi);
+  const auto fedges = f.to_edges();
+
+  for (auto&& e : fedges) {
+    const auto ei = this->find(e);
+
+    if (ei.is_valid()) {
+      // Edge already resitered
+      // So only edge-face connectivity data are updated
+      face_edges_[fi].push_back(ei);
+      edge_faces_[ei].push_back(fi);
+    } else {
+      // Edge is new
+      // So first resiter the new edge, and then update all connectivity data.
+      const edge_index ej{edges_.size()};
+      edges_.push_back(e);
+      edge_faces_.resize(edges_.size());
+
+      edge_faces_[ej].push_back(fi);
+      face_edges_[fi].push_back(ej);
+      vertex_edges_[e.first].push_back(ej);
+      vertex_edges_[e.second].push_back(ej);
+      vertex_vertices_[e.first].push_back(e.second);
+      vertex_vertices_[e.second].push_back(e.first);
+    }
+  }
+}
+
+template <typename Point, typename Face>
+void fracture_mesh<Point, Face>::update_edge_connectivity(
+    const fmesh::edge& e, const edge_index ei) noexcept {
+  // Update vertex-edges and vertex-vertices
+  vertex_edges_[e.first].push_back(ei);
+  vertex_edges_[e.second].push_back(ei);
+  vertex_vertices_[e.first].push_back(e.second);
+  vertex_vertices_[e.second].push_back(e.first);
+
+  // Find faces connected to the edge
+  // Faces connected to both ends of the edge are the ones
+  const auto& fs1 = vertex_faces_[e.first];
+  const auto& fs2 = vertex_faces_[e.second];
+  for (auto&& fi : fs1) {
+    if (std::find(fs2.begin(), fs2.end(), fi) != fs2.end()) {
+      edge_faces_[ei].push_back(fi);
+      face_edges_[fi].push_back(ei);
+    }
+  }
+}
+
+template <typename Point, typename Face>
+void fracture_mesh<Point, Face>::remove_invalid_entities() {
+  if (!this->has_invalid_entities()) return;
+
+  if (has_invalid_vertices_) {
+    //
+  }
+
+  if (has_invalid_edges_) {
+    //
+  }
+
+  if (has_invalid_faces_) {
+    //
+  }
+}
 
 }  // namespace fmesh
