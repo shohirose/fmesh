48f698222a4716399710b29b72457c8784c5b1d4
Sho Hirose <sho.hirose@japex.co.jp>
2019-12-09 15:33:39 +0900

    fix: add explicit keyword to index_iterator's ctor.

--- include/fmesh/index_iterator.hpp
+++ include/fmesh/index_iterator.hpp
@@ -9,8 +9,8 @@
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -42,7 +42,7 @@ class index_iterator {
   using reference = T&;
 
   index_iterator() = default;
-  index_iterator(T idx) : idx_{idx} {}
+  explicit index_iterator(T idx) : idx_{idx} {}
 
   index_iterator& operator++() noexcept {
     ++idx_;
